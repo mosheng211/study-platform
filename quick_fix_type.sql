d3fffe7d919791b4148a087c0564790bc1f2f68f
mosheng211 <1098901025@qq.com>
2025-06-07 21:55:47 +0800

    Initial commit: Laravel Vue.js learning platform

--- quick_fix_type.sql
+++
@@ -1,9 +0,0 @@
--- 快速修复 resources 表的 type 字段问题
--- 将type字段改为VARCHAR类型以支持所有资源类型
-
--- 修改type字段为VARCHAR类型
-ALTER TABLE resources MODIFY COLUMN type VARCHAR(50) NOT NULL DEFAULT 'article';
-
--- 验证修复结果
-SELECT 'Type字段修复完成' as message;
-SHOW COLUMNS FROM resources LIKE 'type'; 
